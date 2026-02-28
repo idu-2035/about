43ff209d83e4d2e64f0d1b90f93d298bed569d7c
idu-2035 <luketretiakov710@gmail.com>
2026-02-28 17:09:23 +0800

    Add files via upload

---
+++ 关于我们-web/src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "@/pages/Home";
+import BrandLogoPage from "@/pages/BrandLogoPage";
+import ConsultationDataPage from "@/pages/ConsultationDataPage";
+import { useState } from "react";
+import { AuthContext } from '@/contexts/authContext';
+
+export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const logout = () => {
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ isAuthenticated, setIsAuthenticated, logout }}
+    >
+      <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/brand" element={<BrandLogoPage />} />
+         <Route path="/consultation-data" element={<ConsultationDataPage />} />
+         <Route path="/other" element={<div className="text-center text-xl">Other Page - Coming Soon</div>} />
+      </Routes>
+    </AuthContext.Provider>
+  );
+}
